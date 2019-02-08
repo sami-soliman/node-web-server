aacf9ceed466d8be553857b478d0b7dc0187dd59
sami <samy.soliman1997@gmail.com>
2019-02-08 16:16:38 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+
+// using bulit in middleWare 
+// app.use(express.static(__dirname + '/public'));
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now}: trying to  ${req.path} `;
+    fs.appendFile('server.log', log + '\n',(err)=>{
+        if(err)
+        console.log("error !!");
+    });
+    next();
+});
+
+hbs.registerHelper('getCurrenrYear',()=>{
+    return new Date().getFullYear();
+});
+
+app.get('/', (req,res)=> {
+    // res.send('<h1> Hello express !</h1>');    // content type text/html
+    /** res.send(   // cotent type json
+        {
+            name : 'sam',
+            likes : ['codeing','cr7']
+        }
+    );*/
+
+    res.render('home.hbs',{
+        pageTitle : 'Home'
+    })
+});
+
+app.get('/about', (req,res)=> {
+    //res.send('<h1> hello from about !</h1>');    // content type text/html
+    res.render('about.hbs',
+    {
+        pageTitle : 'about'
+    });
+});
+
+
+app.listen(3000,()=>{
+    var datetime = new Date();
+    console.log( datetime + " : starting the app at port 3000 ...")
+});
